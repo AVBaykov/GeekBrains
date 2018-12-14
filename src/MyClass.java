b52801125d7a58f834afd777b66c1a665e9421fe
ABaykov <alexandr.baykov@gmail.com>
2018-12-15 00:47:50 +0300

    ДЗ по массивам и циклам

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,62 +1,130 @@
-import java.time.Year;
+import java.util.Arrays;
 
 public class MyClass {
     public static void main(String[] args) {
 
         /**
+         * первый пункт ДЗ
+         */
+        int[] fstArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        for (int i = 0; i < fstArray.length; i++) {
+            if (fstArray[i] == 1) {
+                fstArray[i] = 0;
+            } else {
+                fstArray[i] = 1;
+            }
+        }
+
+        /**
          * второй пункт ДЗ
          */
-        boolean a = true;
-        char b = 'b';
-        byte c = 8;
-        short d = 10;
-        int e = 100;
-        long f = 1000L;
-        float g = 1.0F;
-        double h = 20.5;
-        String string = "строка";
-    }
+        int[] sndArray = new int[8];
 
-    /**
-     * третий пункт ДЗ
-     */
-    static int someFunction(int a, int b, int c, int d) {
-        return a * (b + (c / d));
-    }
+        for (int i = 1; i < sndArray.length; i++) {
+            sndArray[i] = sndArray[i - 1] + 3;
+        }
 
-    /**
-     * четвертый пункт ДЗ
-     */
-    static boolean isSumBetweenTenAndTwenty(int a, int b) {
-        int sum = a + b;
-        return sum >= 10 && sum <= 20;
-    }
+        /**
+         * третий пункт ДЗ
+         */
+        int[] trdArray = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < trdArray.length; i++) {
+            if (trdArray[i] < 6) {
+                trdArray[i] *= 2;
+            }
+        }
 
-    /**
-     * пятый пункт ДЗ
-     */
-    static void positiveOrNegative(int a) {
-        System.out.println(a >= 0 ? "positive" : "negative");
+        /**
+         * четвертый пункт ДЗ
+         */
+        int[][] fourthArray = new int[10][10];
+
+        for (int i = 0; i < fourthArray.length; i++) {
+            fourthArray[i][i] = 1;
+            fourthArray[i][fourthArray.length - i - 1] = 1;
+        }
+
+        System.out.println("Проверка четвертого пункта");
+        for (int i = 0; i < fourthArray.length; i++) {
+            for (int j = 0; j < fourthArray[i].length; j++) {
+                System.out.print(fourthArray[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        /**
+         * пятый пункт ДЗ
+         */
+        System.out.println();
+        System.out.println("Проверка пятого пункта");
+        int min, max;
+        int[] fifthArray = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        Arrays.sort(fifthArray);
+        min = fifthArray[0];
+        max = fifthArray[fifthArray.length - 1];
+        System.out.println(min + " " + max);
+
+
+        /**
+         * проверка 7ого пункта ДЗ
+         */
+        System.out.println();
+        System.out.println("Проверка седьмого пункта");
+        int[] test = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(shift(test, -2)));
     }
 
     /**
      * шестой пункт ДЗ
      */
-    static boolean isNegative(int a) {
-        return a < 0;
+    static boolean checkBalance(int[] array) {
+        if (array != null) {
+            int sum = 0;
+            int partial = 0;
+
+            for (int i : array) {
+                sum += i;
+            }
+
+            for (int i : array) {
+                partial += i;
+                sum -= i;
+                if (partial == sum) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     /**
      * седьмой пункт ДЗ
      */
-    static void pringName(String name) {
-        System.out.printf("Привет, %s!", name);
+    static int[] shift(int[] array, int offset) {
+        if (offset == 0) {
+            return array;
+        }
+        offset = offset % array.length;
+        return offset > 0 ? shiftRight(array, offset) : shiftLeft(array, offset);
     }
 
-    /**
-     * восьмой пункт ДЗ
-     */
-    static void leapYear(int year) {
-        System.out.println(Year.isLeap(year) ? "високосный" : "не високосный");
+    private static int[] shiftRight(int[] array, int offset) {
+        for (int i = 0; i < offset; i++) {
+            int temp = array[array.length - 1];
+            System.arraycopy(array, 0, array, 1, array.length - 1);
+            array[0] = temp;
+        }
+        return array;
     }
+
+    private static int[] shiftLeft(int[] array, int offset) {
+        for (int i = 0; i > offset; i--) {
+            int temp = array[0];
+            System.arraycopy(array, 1, array, 0, array.length - 1);
+            array[array.length - 1] = temp;
+        }
+        return array;
+    }
+
 }
